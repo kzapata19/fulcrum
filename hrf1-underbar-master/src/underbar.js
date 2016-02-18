46d482b1b9968e930af0ba2b7ec348b847f7f831
Karen Zapata <karenzapata@Karens-MacBook-Air.local>
2016-02-17 23:28:32 -0800

    Complete: _.some method

--- hrf1-underbar-master/src/underbar.js
+++ hrf1-underbar-master/src/underbar.js
@@ -241,6 +241,10 @@
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    return _.reduce(collection, function(accumulator, item) {
+        iterator = iterator || _.identity
+        return accumulator || !!iterator(item);
+      }, false);
   };
 
 
