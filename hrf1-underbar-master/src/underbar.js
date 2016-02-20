00cedd999eec961c069ec2efedff91cf087146ee
Karen Zapata <karenzapata@Karens-MacBook-Air.local>
2016-02-20 00:22:51 -0800

    Progress on _.defaults

--- hrf1-underbar-master/src/underbar.js
+++ hrf1-underbar-master/src/underbar.js
@@ -280,6 +280,16 @@
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    _.each(arguments, function(accumObj) {
+      _.each(accumObj, function(value, key) {
+        if (obj[key] === undefined) {
+          obj[key] = value; 
+        }
+      });
+    });
+    
+    return obj
+  };
   };
 
 
