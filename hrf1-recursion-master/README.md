0182189a523176f13beba7fa0c31d9f05e7ffad6
Karen Zapata <karenzapata@Karens-MacBook-Air.local>
2016-04-03 18:07:05 -0700

    updated pluck and uniq

---
+++ hrf1-recursion-master/README.md
@@ -0,0 +1,2 @@
+# Recursion Sprint
+- Review the README associated with this sprint [here](http://fulcrum.hackreactor.com/content/sprint-recursion.html)
--- hrf1-underbar-master/src/underbar.js
+++ hrf1-underbar-master/src/underbar.js
@@ -129,7 +129,8 @@
 
  // Produce a duplicate-free version of the array.
 
-  _.uniq = function(array) {
+  _.uniq = function(array) { 
+  
     var obj = {};
     var uniqKeys = [];
     _.each(array, function(item){
